cd2c72bccec678418f02ecb47a832dd1a66247f9
viparthasarathy <viparthasarathy@gmail.com>
2016-08-17 10:59:12 -0700

    Create SearchBar component and render on index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/SearchBar';
+
 const API_KEY = 'AIzaSyASd00OTAzeDPsozoMZMbl_njwsYsChhic';
 
 const App = () => {
-  return <div>Hi!</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
