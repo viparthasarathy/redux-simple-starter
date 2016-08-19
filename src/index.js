62a3d15def8ff088beb23f3e16d2c3c78f922df5
viparthasarathy <viparthasarathy@gmail.com>
2016-08-19 14:06:56 -0700

    Define constructor function for app component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -6,11 +6,20 @@ import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyASd00OTAzeDPsozoMZMbl_njwsYsChhic';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+
   render () {
     return (
       <div>
