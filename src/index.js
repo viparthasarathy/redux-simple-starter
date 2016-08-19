bba2ea196feefefaa800048af1a3452c780151bb
viparthasarathy <viparthasarathy@gmail.com>
2016-08-19 14:02:54 -0700

    Change app component to class based component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyASd00OTAzeDPsozoMZMbl_njwsYsChhic';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
