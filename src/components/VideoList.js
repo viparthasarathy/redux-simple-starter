421475b394710aeff8fa6128811393e0825279c6
viparthasarathy <viparthasarathy@gmail.com>
2016-08-27 17:30:54 -0700

    Add onVideoClick function to change videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,12 @@ import VideoListItem from './VideoListItem';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
+    return (
+      <VideoListItem
+      onVideoSelect={props.onVideoSelect}
+      key={video.etag}
+      video={video} />
+    );
   });
 
   return (
